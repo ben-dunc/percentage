3cf92b8284b86d0b092505bb125caa68ca0d2f46
benduncan <frankbaconman@gmail.com>
2024-03-06 19:50:11 -0700

    Added animation to increment percentage.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { delay } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'percentage';
+  targetPer = 75;
+  showPerNum = 0;
+  showPerStr: string = "";
+
+
+  ngOnInit() {
+    this.animatePer();
+  }
+
+  async animatePer() {
+    await this.delay(1000);
+    while (this.showPerNum < this.targetPer) {
+      this.showPerNum++;
+      this.showPerStr = this.showPerNum + "%";
+      if (this.showPerStr.length == 2)
+        this.showPerStr = "0" + this.showPerStr;
+      const d = this.calcDelay(this.targetPer - this.showPerNum)
+      await this.delay(d);
+    }
+  }
+
+  calcDelay(difference: number): number {
+    return Math.min(500, (2000 / difference));
+  }
+
+  delay(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
 }
